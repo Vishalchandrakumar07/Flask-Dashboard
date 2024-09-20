44fd1bdc5a95423df384c8fe861988370b83e17c
Vishalchandrakumar07 <vishalchandrakumar07@gmail.com>
2024-09-20 21:19:55 +0530

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -5,10 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Corrected configuration key
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///expensesDB.db"
-app.config["SECRET_KEY"] = (
-    "drvuurrtduyfdvgnfrdvbfdbvcsyjgvtfhtcjhvtdrfdgjfthhhftd234567"
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://default:3RPk1VeSxaCJ@ep-shy-glade-a4o8y8my-pooler.us-east-1.aws.neon.tech:5432/verceldb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "drvuurrtduyfdvgnfrdvbfdbvcsyjgvtfhtcjhvtdrfdgjfthhhftd234567"
+
 
 
 db = SQLAlchemy(app)
